50ce1bffe13065189d4d725387ebe021ed067965
adrianoesch <adrianoesch@gmx.ch>
2018-07-15 19:58:57 +0100

    new download interface

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import json, os, binascii, platform
+import json, os, binascii, platform, zipfile
 from datetime import datetime
-from flask import Flask, send_from_directory, request
+from io import BytesIO
+from flask import Flask, send_from_directory, request, render_template, send_file
 from flask_basicauth import BasicAuth
 
 app  = Flask(__name__)
 
-if platform.system() == 'linux':
+if platform.system().lower() == 'linux':
     app.config['BASIC_AUTH_USERNAME'] = os.environ['SDL_AUTH_USER']
     app.config['BASIC_AUTH_PASSWORD'] = os.environ['SDL_AUTH_PWD']
 else:
@@ -32,10 +33,38 @@ def save():
             f.write(string)
     return json.dumps({'success':True})
 
-@app.route('/download',methods=['GET'])
+@app.route('/download/',methods=['GET'])
+def render_home():
+    return render_download('')
+
+@app.route('/download/<path:path>',methods=['GET'])
 @basic_auth.required
-def downloads():
-    return json.dumps({"success":True})
+def render_download(path):
+    contents = os.listdir(os.path.join('data','csv',path))
+    return render_template('download.html',contents=contents,dir=path)
+
+@app.route('/file/<path:path>',methods=['GET'])
+@basic_auth.required
+def send_single_file(path):
+    return send_file(os.path.join('data','csv',path),as_attachment=True)
+
+@app.route('/zipped/',methods=['GET'])
+@basic_auth.required
+def send_zipped_home():
+    return send_zipped_dir('')
+
+@app.route('/zipped/<path:path>',methods=['GET'])
+@basic_auth.required
+def send_zipped_dir(path):
+    dir = os.path.join('data','csv',path)
+    zipf = BytesIO()
+    zipo = zipfile.ZipFile(zipf,'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            zipo.write(os.path.join(root, file))
+    zipo.close()
+    zipf.seek(0)
+    return send_file(zipf,as_attachment=True,attachment_filename='SDL_downloads.zip')
 
 @app.route('/ip-address',methods=['GET'])
 def get_ip():
---
+++ sdl.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+case "$1" in
+  start)
+    echo -n "Starting SDL: "
+    cd /home/oescha/socialdecisionlab/
+    source env.txt
+    sdl/bin/gunicorn --workers 4 --bind localhost:5000 --daemon --log-file gunicorn.log wsgi:app
+    echo "Success"
+    ;;
+  stop)
+    echo -n "Stopping SDL: "
+    pkill gunicorn
+    echo "Success"
+    ;;
+  restart)
+    # Re-run this script with stop and start arguments.
+    $0 stop
+    sleep 2
+    $0 start
+    ;;
+  reload|force-reload)
+    echo "WARNING reload and force-reload not supported by this script"
+esac
