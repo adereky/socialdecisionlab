87129bf90e51456ef2312be440a35fbff271f3b8
Adrian Oesch <adrianoesch@gmx.ch>
2018-09-07 09:54:43 +0200

    print zip only if condition false

--- public/plugins/jspsych-savezip.js
+++ public/plugins/jspsych-savezip.js
@@ -12,25 +12,27 @@ jsPsych.plugins['savezip'] = (function(){
     plugin.trial = function(display_element, trial){
       if(Experiment.utils.isDataSaved){
         jsPsych.finishTrial();
+      }else{
+        var html = Experiment.utils.wrap('There was an error sending your data to the server. '+
+        'Please download your experiment data and send the zip file to <a href="mailto:adereky@ethz.ch">adereky@ethz.ch</a>.',200)
+
+        display_element.append(html)
+
+        display_element.children().append("<button id='jspsych-savezip-continue-button' style='"+
+            trial.buttonStyle+"'><p>Continue</p></button>")
+        display_element.children().append("<button id='jspsych-savezip-button' onclick='Experiment.utils.saveZip()' style='"+
+            trial.buttonStyle+"margin-right:20px;'><p>Download</p></button>")
+
+        $('#jspsych-savezip-continue-button').on('click',function(){
+          if(confirm('Please confirm that you have sent the downloaded zip file to adereky@ethz.ch.')){
+              jsPsych.finishTrial()
+          }
+        });
+
+        display_element.append(html)
+
       };
 
-      var html = Experiment.utils.wrap('There was an error sending your data to the server. '+
-      'Please download your experiment data and send the zip file to <a href="mailto:adereky@ethz.ch">adereky@ethz.ch</a>.',200)
-
-      display_element.append(html)
-
-      display_element.children().append("<button id='jspsych-savezip-continue-button' style='"+
-          trial.buttonStyle+"'><p>Continue</p></button>")
-      display_element.children().append("<button id='jspsych-savezip-button' onclick='Experiment.utils.saveZip()' style='"+
-          trial.buttonStyle+"margin-right:20px;'><p>Download</p></button>")
-
-      $('#jspsych-savezip-continue-button').on('click',function(){
-        if(confirm('Please confirm that you have sent the downloaded zip file to adereky@ethz.ch.')){
-            jsPsych.finishTrial()
-        }
-      });
-
-      display_element.append(html)
     };
 
     return plugin;
